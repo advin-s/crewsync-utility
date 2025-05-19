e6aa8537a22d287b3611f6b0fe3d536d6d46cc83
advin <adithpsycho@gmail.com>
2025-05-19 10:35:50 +0530

    updates

--- src/crewsync-utility.js
+++ src/crewsync-utility.js
@@ -1,4 +1,9 @@
 // Anything exported from this file is importable by other in-browser modules.
+import { BehaviorSubject } from 'rxjs'
+
 export function publicApiFunction() {
     console.log('utility called !!!!!')
 }
+
+
+export const user$ = new BehaviorSubject({})
